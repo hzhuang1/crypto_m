07e5976cdd38952759e33fa122b23ee8647179eb
Haojian Zhuang <haojian.zhuang@gmail.com>
2023-05-12 13:46:42 +0800

    ssl: test aes-128-cbc with openssl command
    Encrypt and decrypt data with openssl command. Test result matches
    the source data.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>

--- ssl/test.sh
+++ ssl/test.sh
@@ -2,4 +2,10 @@
 
 ALG="md5"
 
+echo "MD5:"
 echo -n hello | openssl md5 -c
+
+echo "AES-128-CBC"
+openssl enc -aes-128-cbc -K e0e0e0e0f1f1f1f1 -iv 3031323334353637 -in message.plain -out message.enc
+openssl enc -aes-128-cbc -K e0e0e0e0f1f1f1f1 -iv 3031323334353637 -d -in message.enc -out message.out
+diff -puNr message.plain message.out

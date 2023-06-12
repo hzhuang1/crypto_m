44c6ca5c504644443eb44642e4cb70215228a934
Haojian Zhuang <haojian.zhuang@gmail.com>
2023-06-12 23:10:43 +0800

    crypto_m: make key size updated easily in run script
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>

--- kernel/crypto_m/run.sh
+++ kernel/crypto_m/run.sh
@@ -2,11 +2,12 @@
 BUF_SIZE=131072
 ALG=$1
 #DECRYPT=1
+KBITS=128
 echo "Encrypt"
 for i in 256 512 1024 2048 4096 8192 16384 32768 65536 131072
 do
 	echo "insert module for $ALG with $i-byte"
-	sudo insmod ./hash_m.ko alg_name=$ALG buf_size=$i key_bits=128 encrypt_mode=1
+	sudo insmod ./hash_m.ko alg_name=$ALG buf_size=$i key_bits=$KBITS encrypt_mode=1
 	sudo rmmod hash_m
 done
 if [ ! -z $DECRYPT ]; then
@@ -14,7 +15,7 @@ if [ ! -z $DECRYPT ]; then
 	for i in 256 512 1024 2048 4096 8192 16384 32768 65536 131072
 	do
 		echo "insert module for $ALG with $i-byte"
-		sudo insmod ./hash_m.ko alg_name=$ALG buf_size=$i key_bits=128 encrypt_mode=0
+		sudo insmod ./hash_m.ko alg_name=$ALG buf_size=$i key_bits=$KBITS encrypt_mode=0
 		echo "remove module"
 		sudo rmmod hash_m
 		sudo dmesg | tail -n 3

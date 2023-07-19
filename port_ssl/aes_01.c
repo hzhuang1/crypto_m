0b85eeb6b30ad92f3d847db01fd4b1311a6fac1d
Haojian Zhuang <haojian.zhuang@gmail.com>
2023-07-19 14:55:07 +0800

    port_ssl: pass 128b test vec on assembly implementation
    The assembly implementation include two functions. One is to expand
    key, and the other is to call encrypt.
    
      * The removed EOR instructions in encrypt function is restored.
      * The C implementation also needs to expand key. But there's still
    some issue in it.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>

--- port_ssl/aes_01.c
+++ port_ssl/aes_01.c
@@ -7,6 +7,8 @@
 
 #define OUT_BUF_LEN	4096
 
+extern void aes_v8_set_encrypt_key(const unsigned char *userKey,
+				   const int bits, AES_KEY *key);
 extern void aes_cbc_01(const unsigned char *inp, unsigned char *out,
 			size_t length, const AES_KEY *key,
 			unsigned char *ivp, const int enc);
@@ -19,8 +21,12 @@ extern void aes_v8_cbc_encrypt(const unsigned char *inp, unsigned char *out,
 
 #define U64(c)		c##ULL
 
+#define GETU32(pt) (((u32)(pt)[0] << 24) ^ ((u32)(pt)[1] << 16) ^ ((u32)(pt)[2] <<  8) ^ ((u32)(pt)[3]))
+#define PUTU32(ct, st) { (ct)[0] = (u8)((st) >> 24); (ct)[1] = (u8)((st) >> 16); (ct)[2] = (u8)((st) >>  8); (ct)[3] = (u8)(st); }
+
 typedef unsigned long long int u64;
 typedef unsigned int u32;
+typedef unsigned char u8;
 
 typedef void (*block128_f) (const unsigned char in[16],
                             unsigned char out[16], const void *key);
@@ -32,6 +38,46 @@ typedef union {
 } uni;
 
 static char *vec_out = NULL;
+
+static const u8 Te4[256] = {
+    0x63U, 0x7cU, 0x77U, 0x7bU, 0xf2U, 0x6bU, 0x6fU, 0xc5U,
+    0x30U, 0x01U, 0x67U, 0x2bU, 0xfeU, 0xd7U, 0xabU, 0x76U,
+    0xcaU, 0x82U, 0xc9U, 0x7dU, 0xfaU, 0x59U, 0x47U, 0xf0U,
+    0xadU, 0xd4U, 0xa2U, 0xafU, 0x9cU, 0xa4U, 0x72U, 0xc0U,
+    0xb7U, 0xfdU, 0x93U, 0x26U, 0x36U, 0x3fU, 0xf7U, 0xccU,
+    0x34U, 0xa5U, 0xe5U, 0xf1U, 0x71U, 0xd8U, 0x31U, 0x15U,
+    0x04U, 0xc7U, 0x23U, 0xc3U, 0x18U, 0x96U, 0x05U, 0x9aU,
+    0x07U, 0x12U, 0x80U, 0xe2U, 0xebU, 0x27U, 0xb2U, 0x75U,
+    0x09U, 0x83U, 0x2cU, 0x1aU, 0x1bU, 0x6eU, 0x5aU, 0xa0U,
+    0x52U, 0x3bU, 0xd6U, 0xb3U, 0x29U, 0xe3U, 0x2fU, 0x84U,
+    0x53U, 0xd1U, 0x00U, 0xedU, 0x20U, 0xfcU, 0xb1U, 0x5bU,
+    0x6aU, 0xcbU, 0xbeU, 0x39U, 0x4aU, 0x4cU, 0x58U, 0xcfU,
+    0xd0U, 0xefU, 0xaaU, 0xfbU, 0x43U, 0x4dU, 0x33U, 0x85U,
+    0x45U, 0xf9U, 0x02U, 0x7fU, 0x50U, 0x3cU, 0x9fU, 0xa8U,
+    0x51U, 0xa3U, 0x40U, 0x8fU, 0x92U, 0x9dU, 0x38U, 0xf5U,
+    0xbcU, 0xb6U, 0xdaU, 0x21U, 0x10U, 0xffU, 0xf3U, 0xd2U,
+    0xcdU, 0x0cU, 0x13U, 0xecU, 0x5fU, 0x97U, 0x44U, 0x17U,
+    0xc4U, 0xa7U, 0x7eU, 0x3dU, 0x64U, 0x5dU, 0x19U, 0x73U,
+    0x60U, 0x81U, 0x4fU, 0xdcU, 0x22U, 0x2aU, 0x90U, 0x88U,
+    0x46U, 0xeeU, 0xb8U, 0x14U, 0xdeU, 0x5eU, 0x0bU, 0xdbU,
+    0xe0U, 0x32U, 0x3aU, 0x0aU, 0x49U, 0x06U, 0x24U, 0x5cU,
+    0xc2U, 0xd3U, 0xacU, 0x62U, 0x91U, 0x95U, 0xe4U, 0x79U,
+    0xe7U, 0xc8U, 0x37U, 0x6dU, 0x8dU, 0xd5U, 0x4eU, 0xa9U,
+    0x6cU, 0x56U, 0xf4U, 0xeaU, 0x65U, 0x7aU, 0xaeU, 0x08U,
+    0xbaU, 0x78U, 0x25U, 0x2eU, 0x1cU, 0xa6U, 0xb4U, 0xc6U,
+    0xe8U, 0xddU, 0x74U, 0x1fU, 0x4bU, 0xbdU, 0x8bU, 0x8aU,
+    0x70U, 0x3eU, 0xb5U, 0x66U, 0x48U, 0x03U, 0xf6U, 0x0eU,
+    0x61U, 0x35U, 0x57U, 0xb9U, 0x86U, 0xc1U, 0x1dU, 0x9eU,
+    0xe1U, 0xf8U, 0x98U, 0x11U, 0x69U, 0xd9U, 0x8eU, 0x94U,
+    0x9bU, 0x1eU, 0x87U, 0xe9U, 0xceU, 0x55U, 0x28U, 0xdfU,
+    0x8cU, 0xa1U, 0x89U, 0x0dU, 0xbfU, 0xe6U, 0x42U, 0x68U,
+    0x41U, 0x99U, 0x2dU, 0x0fU, 0xb0U, 0x54U, 0xbbU, 0x16U
+};
+static const u32 rcon[] = {
+    0x01000000, 0x02000000, 0x04000000, 0x08000000,
+    0x10000000, 0x20000000, 0x40000000, 0x80000000,
+    0x1B000000, 0x36000000, /* for 128-bit blocks, Rijndael never uses more than 10 rcon values */
+};
 
 void init_buf(void)
 {
@@ -359,6 +405,107 @@ void AES_cbc_encrypt(const unsigned char *in, unsigned char *out,
 */
 }
 
+/**
+ * Expand the cipher key into the encryption key schedule.
+ */
+int AES_set_encrypt_key(const unsigned char *userKey, const int bits,
+                        AES_KEY *key)
+{
+    u32 *rk;
+    int i = 0;
+    u32 temp;
+
+    if (!userKey || !key)
+        return -1;
+    if (bits != 128 && bits != 192 && bits != 256)
+        return -2;
+
+    rk = key->rd_key;
+
+    if (bits == 128)
+        key->rounds = 10;
+    else if (bits == 192)
+        key->rounds = 12;
+    else
+        key->rounds = 14;
+
+    rk[0] = GETU32(userKey     );
+    rk[1] = GETU32(userKey +  4);
+    rk[2] = GETU32(userKey +  8);
+    rk[3] = GETU32(userKey + 12);
+    if (bits == 128) {
+        while (1) {
+            temp  = rk[3];
+            rk[4] = rk[0] ^
+                ((u32)Te4[(temp >> 16) & 0xff] << 24) ^
+                ((u32)Te4[(temp >>  8) & 0xff] << 16) ^
+                ((u32)Te4[(temp      ) & 0xff] << 8) ^
+                ((u32)Te4[(temp >> 24)       ]) ^
+                rcon[i];
+            rk[5] = rk[1] ^ rk[4];
+            rk[6] = rk[2] ^ rk[5];
+            rk[7] = rk[3] ^ rk[6];
+            if (++i == 10) {
+                return 0;
+            }
+            rk += 4;
+        }
+    }
+    rk[4] = GETU32(userKey + 16);
+    rk[5] = GETU32(userKey + 20);
+    if (bits == 192) {
+        while (1) {
+            temp = rk[ 5];
+            rk[ 6] = rk[ 0] ^
+                ((u32)Te4[(temp >> 16) & 0xff] << 24) ^
+                ((u32)Te4[(temp >>  8) & 0xff] << 16) ^
+                ((u32)Te4[(temp      ) & 0xff] << 8) ^
+                ((u32)Te4[(temp >> 24)       ]) ^
+                rcon[i];
+            rk[ 7] = rk[ 1] ^ rk[ 6];
+            rk[ 8] = rk[ 2] ^ rk[ 7];
+            rk[ 9] = rk[ 3] ^ rk[ 8];
+            if (++i == 8) {
+                return 0;
+            }
+            rk[10] = rk[ 4] ^ rk[ 9];
+            rk[11] = rk[ 5] ^ rk[10];
+            rk += 6;
+        }
+    }
+    rk[6] = GETU32(userKey + 24);
+    rk[7] = GETU32(userKey + 28);
+    if (bits == 256) {
+        while (1) {
+            temp = rk[ 7];
+            rk[ 8] = rk[ 0] ^
+                ((u32)Te4[(temp >> 16) & 0xff] << 24) ^
+                ((u32)Te4[(temp >>  8) & 0xff] << 16) ^
+                ((u32)Te4[(temp      ) & 0xff] << 8) ^
+                ((u32)Te4[(temp >> 24)       ]) ^
+                rcon[i];
+            rk[ 9] = rk[ 1] ^ rk[ 8];
+            rk[10] = rk[ 2] ^ rk[ 9];
+            rk[11] = rk[ 3] ^ rk[10];
+            if (++i == 7) {
+                return 0;
+            }
+            temp = rk[11];
+            rk[12] = rk[ 4] ^
+                ((u32)Te4[(temp >> 24)       ] << 24) ^
+                ((u32)Te4[(temp >> 16) & 0xff] << 16) ^
+                ((u32)Te4[(temp >>  8) & 0xff] << 8) ^
+                ((u32)Te4[(temp      ) & 0xff]);
+            rk[13] = rk[ 5] ^ rk[12];
+            rk[14] = rk[ 6] ^ rk[13];
+            rk[15] = rk[ 7] ^ rk[14];
+
+            rk += 8;
+        }
+    }
+    return 0;
+}
+
 void t_cbc_01(int i)
 {
 	struct cipher_testvec *vec = aes_cbc_tv_template;
@@ -373,15 +520,24 @@ void t_cbc_01(int i)
 	}
 	init_buf();
 	memset(iv, 0, 32);
-	//memcpy(iv, vec[i].iv, 16);
+	memcpy(iv, vec[i].iv, 16);
 	memset(&key, 0, sizeof(AES_KEY));
-	memcpy(key.rd_key, vec[i].key, vec[i].klen);
-	key.rounds = vec[i].klen / 4 + 6;
-	printf("key:\n");
-	dump_buf(key.rd_key, 16);
+	//memcpy(key.rd_key, vec[i].key, vec[i].klen);
+	//key.rounds = vec[i].klen / 4 + 6;
+	printf("ptext:\n");
+	dump_buf(vec[i].ptext, 16);
+	printf("user key:\n");
+	dump_buf(vec[i].key, 16);
+	printf("iv:\n");
+	dump_buf(iv, 16);
+	aes_v8_set_encrypt_key(vec[i].key, vec[i].len * 8, &key);
 	printf("klen:%d, rounds:%d\n", vec[i].klen, key.rounds);
+	printf("key:\n");
+	dump_buf(key.rd_key, 240);
 	aes_cbc(vec[i].ptext, vec_out, vec[i].len, &key,
 		iv, 1);
+	printf("ivout:\n");
+	dump_buf(iv, 16);
 	printf("ctext:\n");
 	dump_buf(vec_out, vec[i].len);
 	free_buf();
@@ -393,6 +549,7 @@ void t_cbc_02(int i)
 	int cnt = ARRAY_SIZE(aes_cbc_tv_template);
 	char iv[32];
 	AES_KEY key;
+	int ret;
 
 	if (i >= cnt) {
 		printf("Index %d is too large.\n", i);
@@ -401,15 +558,28 @@ void t_cbc_02(int i)
 	init_buf();
 
 	memset(iv, 0, 32);
-	//memcpy(iv, vec[i].iv, 16);
+	memcpy(iv, vec[i].iv, 16);
 	memset(&key, 0, sizeof(AES_KEY));
-	memcpy(key.rd_key, vec[i].key, vec[i].klen);
-	key.rounds = vec[i].klen / 4 + 6;
-	printf("key:\n");
-	dump_buf(key.rd_key, 16);
+	//memcpy(key.rd_key, vec[i].key, vec[i].klen);
+	//key.rounds = vec[i].klen / 4 + 6;
+	printf("ptext:\n");
+	dump_buf(vec[i].ptext, 16);
+	printf("iv:\n");
+	dump_buf(iv, 16);
+	printf("user key:\n");
+	dump_buf(vec[i].key, 16);
+	ret = AES_set_encrypt_key(vec[i].key, vec[i].klen * 8, &key);
+	if (ret) {
+		printf("set key error:%d\n", ret);
+		return;
+	}
 	printf("klen:%d, rounds:%d\n", vec[i].klen, key.rounds);
+	printf("key:\n");
+	dump_buf(key.rd_key, 240);
 	AES_cbc_encrypt(vec[i].ptext, vec_out, vec[i].len, &key,
 			iv, 1);
+	printf("ivout:\n");
+	dump_buf(iv, 16);
 	printf("ctext:\n");
 	dump_buf(vec_out, vec[i].len);
 	free_buf();
